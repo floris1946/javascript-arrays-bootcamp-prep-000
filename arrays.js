411f2f5744ff79c59d1e3c5e69e73afc6ab2f512
Florisvan der Breggen <florisvanderbreggen@gmail.com>
2018-03-08 07:30:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,37 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
 }
 
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length - 1);
+}
+
 var cities = ["New York", "San Francisco"]
 
 //console.log(destructivelyAddElementToBeginningOfArray(chocolateBars, 'foo'))
